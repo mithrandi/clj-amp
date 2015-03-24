b4d56ae3497acf80947f13cbb6fdcee671ead2aa
Tristan Seligmann <mithrandi@mithrandi.net>
2015-03-24 22:04:29 +0200

    Restructure box tests.

--- test/clj_amp/encoding_test.clj
+++ test/clj_amp/encoding_test.clj
@@ -5,20 +5,14 @@
             [byte-streams :refer [bytes=]]))
 
 (deftest roundtrip-tests
-  (let [byte-specimens [(to-byte-buffer
-                         [0x00 0x01 0x61 0x00 0x01 0x40 0x00 0x01 0x62 0x00
-                          0x03 0x62 0x61 0x62 0x00 0x00])]
-        box-specimens [{}
-                       {"a" [(to-byte-buffer "b")]}
-                       {"\0\0\0" [(to-byte-buffer [0 0 0])]}]
-        roundtrip (comp to-byte-buffer
-                        (partial encode ampbox-codec)
-                        (partial decode ampbox-codec))
-        roundtrip2 (comp (partial decode ampbox-codec)
-                         (partial encode ampbox-codec))]
-    (testing "Box decode->encode roundtripping"
-      (doseq [specimen byte-specimens]
-        (is (bytes= specimen (roundtrip specimen)))))
-    (testing "Box encode->decode roundtripping"
-      (doseq [specimen box-specimens]
-        (is (= specimen (roundtrip2 specimen)))))))
+  (let [specimens [[(to-byte-buffer
+                     [0x00 0x01 0x61 0x00 0x01 0x40 0x00 0x01 0x62 0x00
+                      0x03 0x62 0x61 0x62 0x00 0x00])
+                    {"a" [(to-byte-buffer "@")]
+                     "b" [(to-byte-buffer "bab")]}]]
+        encode-box (comp to-byte-buffer (partial encode ampbox-codec))
+        decode-box (partial decode ampbox-codec)]
+    (testing "Box encoding verification"
+      (doseq [[encoded decoded] specimens]
+        (is (= decoded (decode-box encoded)))
+        (is (bytes= encoded (encode-box decoded)))))))
