17bf900dd11274a2ec23946489d5bb762f00ada2
Tristan Seligmann <mithrandi@mithrandi.net>
2015-03-24 20:48:04 +0200

    Refactor types some more.

--- src/clj_amp/encoding.clj
+++ src/clj_amp/encoding.clj
@@ -1,14 +1,25 @@
 (ns clj-amp.encoding
-  (:require [gloss.core :refer [defcodec delimited-frame repeated
-                                ordered-map compile-frame string]]
+  (:require [gloss.core :refer
+             [defcodec repeated compile-frame string finite-block]]
             [gloss.core.codecs :refer [wrap-suffixed-codec]]))
 
-(defcodec ampbox-codec
-  (wrap-suffixed-codec
-   [0 0]
-   (compile-frame
-    (repeated
-     (ordered-map :key (string "iso-8859-1" :prefix :uint16-be)
-                  :value (repeated :byte :prefix :uint16-be))
-     :prefix :none))))
+
+(defcodec ampbox-key
+  (string "iso-8859-1" :prefix :uint16-be))
+
+
+(defcodec ampbox-value
+  (finite-block :uint16-be))
+
+
+(def ampbox-codec
+  (compile-frame
+   (wrap-suffixed-codec
+    "\0\0"
+    (compile-frame
+     (repeated
+      [ampbox-key ampbox-value]
+      :prefix :none)))
+   (partial into [])
+   (partial into {})))
                    
