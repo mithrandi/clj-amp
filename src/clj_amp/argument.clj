760d45ac3cd0191939b62fe3b8cf57368e28bce2
Tristan Seligmann <mithrandi@mithrandi.net>
2015-03-26 01:51:38 +0200

    Start working on arguments.

---
+++ src/clj_amp/argument.clj
@@ -0,0 +1,59 @@
+(ns clj-amp.argument
+  (:require [gloss.core :as g]
+            [gloss.io]))
+
+
+(defmacro defcommand
+  "Define an AMP Command.
+  Usage: (defcommand
+          name
+          command-name?
+          {argument-name {:type argument-type
+                          :name amp-argument-name?
+                          ...}
+           ...}
+           return-value)"
+  ([defname arguments return-value]
+   `(def ~defname (build-command ~(name defname) ~arguments ~return-value)))
+  ([defname command-name arguments return-value]
+   `(def ~defname (build-command ~command-name ~arguments ~return-value))))
+
+
+(defmulti to-box
+  "Serialize a Command argument to a map of Box keys/values."
+  (fn [argument value] (:type argument)))
+
+
+(defmethod to-box ::bytes
+  [argument value]
+  {(:name argument) value})
+
+
+(defmulti from-box
+  "Deserialize a Command argument from a Box."
+  (fn [argument box] (:type argument)))
+
+
+(defmethod from-box ::bytes
+  [argument box]
+  (get box (:name argument)))
+
+
+(defn defargument
+  "Define an AMP argument type using a gloss codec."
+  [type codec]
+  (defmethod to-box type
+     [argument value]
+     {(:name argument) (gloss.io/encode codec value)})
+  (defmethod from-box type
+     [argument box]
+     (gloss.io/decode codec (get box (:name argument)))))
+
+
+;; TODO: Make this use a BigNum if necessary
+(defargument ::integer
+  (g/compile-frame (g/string-integer :ascii)))
+
+
+(defargument ::string
+  (g/compile-frame (g/string :utf-8)))
---
+++ test/clj_amp/argument_test.clj
@@ -0,0 +1,35 @@
+(ns clj-amp.argument-test
+  (:require [clojure.test :refer :all]
+            [clj-amp.argument :as a :refer :all]
+            [clj-amp.box :refer [boxes=]]))
+
+;; (deftest command-definition
+;;   (testing "Command definition"
+;;     (is (= my-cool-command
+;;            (build-command "my-cool-command" {} :foo)
+;;            {:name "my-cool-command"
+;;             :arguments {}
+;;             :return-value :foo}))))
+
+
+(defn roundtrips
+  [argument value serialized]
+  (is (= value
+         (from-box argument serialized)))
+  (is (boxes= serialized
+              (to-box argument value))))
+
+
+(deftest argument-serialization
+  (testing "integer"
+    (roundtrips {:type ::a/integer :name "int"}
+                42
+                {"int" "42"}))
+  (testing "bytes"
+    (roundtrips {:type ::a/bytes :name "bytes"}
+                (gloss.io/to-byte-buffer [0x00 0x01 0x02])
+                {"bytes" (gloss.io/to-byte-buffer [0x00 0x01 0x02])}))
+  (testing "string"
+    (roundtrips {:type ::a/string :name "str"}
+                "x: \u2603"
+                {"str" (gloss.io/to-byte-buffer (byte-array [0x78 0x3a 0x20 0xe2 0x98 0x83]))})))
