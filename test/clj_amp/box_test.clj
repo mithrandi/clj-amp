df2670960d05d9677b688812443bb774a1d9467e
Tristan Seligmann <mithrandi@mithrandi.net>
2015-03-25 18:05:51 +0200

    Switch to prefixed names for some stuff.

--- test/clj_amp/box_test.clj
+++ test/clj_amp/box_test.clj
@@ -1,8 +1,8 @@
 (ns clj-amp.box-test
   (:require [clojure.test :refer :all]
             [clj-amp.box :refer :all]
-            [gloss.io :refer [encode decode to-byte-buffer]]
-            [byte-streams :refer [bytes= to-byte-array]]
+            [gloss.io]
+            [byte-streams :as bs]
             [slingshot.test]
             [plumbing.core :refer [map-vals]]))
 
@@ -11,30 +11,31 @@
 
 (defn boxes=
   [box1 box2]
-  (on- = (partial map-vals to-byte-buffer) box1 box2))
+  (on- = (partial map-vals bs/to-byte-buffer) box1 box2))
   ;; (every? #(= (->> %1 (get box1) (to-byte-buffer))
   ;;             (->> %1 (get box2) (to-byte-buffer)))
   ;;         (concat (keys box1) (keys box2))))
 
 
 (deftest roundtrip-tests
-  (let [specimens [[(to-byte-buffer
+  (let [specimens [[(bs/to-byte-buffer
                      [0x00 0x01 0x61 0x00 0x01 0x40 0x00 0x01 0x62 0x00
                       0x03 0x62 0x61 0x62 0x00 0x00])
                     {"a" "@" "b" "bab"}]
-                   [(to-byte-buffer
+                   [(bs/to-byte-buffer
                      [0x00 0x01 0x00 0x00 0x01 0x00 0x00 0x00])
-                    {"\0" (to-byte-buffer [0x00])}]
+                    {"\0" (bs/to-byte-buffer [0x00])}]
                    [(byte-array [0x00 0x00])
                     {}]]
-        encode-box (comp to-byte-buffer (partial encode ampbox-codec))
-        decode-box (partial decode ampbox-codec)]
+        encode-box (comp bs/to-byte-buffer
+                         (partial gloss.io/encode ampbox-codec))
+        decode-box (partial gloss.io/decode ampbox-codec)]
     (testing "Box encoding verification"
       (doseq [[encoded decoded] specimens]
         (is (boxes= decoded (decode-box encoded)))
-        (is (bytes= encoded (encode-box decoded)))
+        (is (bs/bytes= encoded (encode-box decoded)))
         (is (boxes= decoded (-> decoded encode-box decode-box)))
-        (is (bytes= encoded (-> encoded decode-box encode-box)))))))
+        (is (bs/bytes= encoded (-> encoded decode-box encode-box)))))))
 
 
 (deftest invalid-boxes
