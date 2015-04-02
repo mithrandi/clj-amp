cdfb5ed5915b7bf704d5d52372f2ec80e115ff6d
Tristan Seligmann <mithrandi@mithrandi.net>
2015-04-02 03:11:16 +0200

    Fix version.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject clj-amp/clj-amp "0.9.1-SNAPSHOT"
+(defproject clj-amp/clj-amp "0.9.0-SNAPSHOT"
   :description "Clojure implementation of AMP"
   :url "https://github.com/mithrandi/clj-codetip"
   :license {:name "Expat (MIT) license"
