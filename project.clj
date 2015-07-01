40dc1214f265710f4a3f2fdcd0044a508c95b115
Tristan Seligmann <mithrandi@mithrandi.net>
2015-07-01 23:37:27 +0200

    Bump dependencies again, since Clojure 1.7.0 is out.

--- project.clj
+++ project.clj
@@ -3,11 +3,11 @@
   :url "https://github.com/mithrandi/clj-codetip"
   :license {:name "Expat (MIT) license"
             :url "http://spdx.org/licenses/MIT"}
-  :dependencies [[org.clojure/clojure "1.7.0-beta1"]
+  :dependencies [[org.clojure/clojure "1.7.0"]
                  [gloss "0.2.5"]
                  [aleph "0.4.0"]
                  [slingshot "0.12.2"]
-                 [prismatic/plumbing "0.4.2"]
+                 [prismatic/plumbing "0.4.4"]
                  [clj-time "0.9.0"]
                  [manifold "0.1.0"]]
   :deploy-repositories [["releases" :clojars]]
